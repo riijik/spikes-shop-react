83e262ff26efc3f4d35d658e2ce8b735da22ca2f
iyakr <iya.krivtsova@gmail.com>
2021-10-15 19:53:00 +0300

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import spikeLogo from './img/spikeLogo.png'
+import shopLogo from './img/shopLogo.png'
+import userLogo from './img/userLogo.png'
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img  src={spikeLogo} className="spikeLogo"/>
+          <div className="headerInfo">
+            <h3>Spikes shop</h3>
+            <p>Best shoes for running</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <img src={shopLogo} className="shopLogo"/>
+            <span>0 rub.</span>
+          </li>
+          <li>
+            <img src={userLogo} className="userLogo"/>
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1>All shoes</h1>
+        ...
+      </div>
     </div>
   );
 }
-
-export default App;
